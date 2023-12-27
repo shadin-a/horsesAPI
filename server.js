fa2f4e86ce5cf230232f8ee7f54ff5e44b0ddf86
Shadin Al-Arab <shadin.alarab@gmail.com>
2023-12-27 09:49:20 -0800

    api server setup

---
+++ server.js
@@ -0,0 +1,42 @@
+//REQUIRES
+
+const express = require("express"); 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+   await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// For testing purposes 
+app.get("/", (req, res) => { 
+    res.send("<h2>It's Working!</h2>"); 
+}); 
+
+app.listen(PORT, () => { 
+    console.log(`API is listening on port ${PORT}`); 
+});
