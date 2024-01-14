3c31926124a3599ef7e3ede345c5f76a77531752
Shadin Al-Arab <shadin.alarab@gmail.com>
2024-01-13 16:19:14 -0800

    get owners and locations by id

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -1,6 +1,8 @@
 const client = require("../../connection.js");
 const router = require("express").Router();
+const { ObjectId } = require('mongodb');
 
+//GET ALL LOCATIONS
 router.get('/', async (req, res) => {
 
     const cursor = client.db("horseDatabase").collection("locations").find();
@@ -9,12 +11,26 @@ router.get('/', async (req, res) => {
     res.json({ locationData })
 });
 
+//GET LOCATION BY ID
+router.get('/:id', async (req, res) => {
+    try {
+      const locationId = req.params.id;
+      const objectId = new ObjectId(locationId);
+  
+      const cursor = await client.db("horseDatabase").collection('locations').findOne({ _id: objectId });
+  
+      if (cursor) {
+        res.json(cursor);
+      } else {
+        res.status(404).json({ error: "Location not found" });
+      }
+    } catch (error) {
+      console.error("Error fetching location data:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+
 
 module.exports = router
 
-// router.get("/:id", async (req,res) => {
-//     const cursor = await client.db("horseDatabase").collection('owners').findOne({_id: req.params.id})
-//     //const oneOwner = await cursor.toArray();
-//     console.log(cursor);
-//     res.json(cursor);
-//   });
--- routes/api/ownerRoutes.js
+++ routes/api/ownerRoutes.js
@@ -1,12 +1,33 @@
 const router = require("express").Router();
 const client = require("../../connection.js");
+const { ObjectId } = require('mongodb');
 
+//GET ALL OWNERS
 router.get('/', async (req, res) => {
    
     const cursor = client.db("horseDatabase").collection("owners").find();
     const ownerData = await cursor.toArray();
     console.log("um...hi?", ownerData );
     res.json({ownerData})
+  });
+
+  //ROUTE TO GET ONE OWNER BY ID
+  router.get('/:id', async (req, res) => {
+    try {
+      const ownerId = req.params.id;
+      const objectId = new ObjectId(ownerId);
+  
+      const cursor = await client.db("horseDatabase").collection('owners').findOne({ _id: objectId });
+  
+      if (cursor) {
+        res.json(cursor);
+      } else {
+        res.status(404).json({ error: "Owner not found" });
+      }
+    } catch (error) {
+      console.error("Error fetching owner data:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   });
 
 
