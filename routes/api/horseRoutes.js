c9cab0c18bb3bf118fc567e632866ba1fef68c68
Shadin Al-Arab <shadin.alarab@gmail.com>
2024-01-13 16:39:23 -0800

    get all horses with same owner

--- routes/api/horseRoutes.js
+++ routes/api/horseRoutes.js
@@ -4,56 +4,50 @@ const { ObjectId } = require('mongodb');
 
 //ROUTE TO GET ALL HORSES
 router.get('/', async (req, res) => {
-   
-    const cursor = client.db("horseDatabase").collection("horses").find();
-    const horsesData = await cursor.toArray();
-    console.log("um...hi?", horsesData);
-    res.json({horsesData})
-   
-  });
+
+  const cursor = client.db("horseDatabase").collection("horses").find();
+  const horsesData = await cursor.toArray();
+  console.log("um...hi?", horsesData);
+  res.json({ horsesData })
+
+});
 
 //ROUTE TO GET ONE HORSE BY ID
-  router.get('/:id', async (req, res) => {
-    try {
-      const horseId = req.params.id;
-      const objectId = new ObjectId(horseId);
-  
-      const cursor = await client.db("horseDatabase").collection('horses').findOne({ _id: objectId });
-  
-      if (cursor) {
-        res.json(cursor);
-      } else {
-        res.status(404).json({ error: "Horse not found" });
-      }
-    } catch (error) {
-      console.error("Error fetching horse data:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+router.get('/:id', async (req, res) => {
+  try {
+    const horseId = req.params.id;
+    const objectId = new ObjectId(horseId);
+
+    const cursor = await client.db("horseDatabase").collection('horses').findOne({ _id: objectId });
+
+    if (cursor) {
+      res.json(cursor);
+    } else {
+      res.status(404).json({ error: "Horse not found" });
     }
-  });
+  } catch (error) {
+    console.error("Error fetching horse data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-  
-  // router.get("/:id", async (req,res) => {
-  //   const cursor = await client.db("horseDatabase").collection('horses').findOne({_id: req.params.id})
-  //   //const oneHorse = await cursor.toArray();
-  //   console.log(cursor);
-  //   res.json(cursor);
-  // });
-// async function findOneHorse(client, nameOfHorse) {
-//     const result = await client.db("horseDatabase").collection("horses").findOne({ name: nameOfHorse })
-//     if (result) {
-//         console.log(`Here is the horse you're looking for:`);
-//         console.log(result);
-//     } else {
-//         console.log(`No horses for you :(`);
-//     }
-// }
+//GET ROUTE FOR ALL HORSES WITH SAME OWNER
+router.get('/owner/:owner', async (req, res) => {
+  try {
+    const owner = req.params.owner;
+    const objectId = new ObjectId(owner);
 
-// app.get('/horses', (req, res) => {
-//     var db = client.db('horseDatabase');
-//     db.collection('horses').find().toArray( (err, results) => {
-//       res.send(results)
-//     });
-// });
+    const cursor = await client.db("horseDatabase").collection('horses').find({ owner: objectId }).toArray();
+    if (cursor.length > 0) {
+      res.json(cursor);
+    } else {
+      res.status(404).json({ error: "No horses found for the specified owner" });
+    }
+  } catch (error) {
+    console.error("Error fetching horse data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 
 
