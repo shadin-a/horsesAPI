2a58907300906c2ea0c3dc620504f4437a5ce539
Shadin Al-Arab <shadin.alarab@gmail.com>
2024-01-16 20:46:24 -0800

    location rendered correctly from object

--- client/src/components/ViewOneHorse.js
+++ client/src/components/ViewOneHorse.js
@@ -3,49 +3,59 @@ import { useParams } from 'react-router-dom';
 
 function ViewOneHorse() {
 
-    const [horse, setHorses] = useState([]);
-    const [owner, setOwner] = useState({})
-    const { id } = useParams();
+  const [horse, setHorses] = useState([]);
+  const [owner, setOwner] = useState({});
+  const [location, setLocation] = useState({})
+  const { id } = useParams();
 
-    useEffect(() => {
-        fetchOneHorse();
-    }, []);
+  useEffect(() => {
+    fetchOneHorse();
+  }, []);
 
-    useEffect(() => {
-        console.log(horse)
-    }, [horse]);
+  useEffect(() => {
+    console.log(horse)
+  }, [horse]);
 
-    const fetchOneHorse = async () => {
-        try {
-            const response = await fetch(`/horses/${id}`);
-            const data = await response.json();
-            setHorses(data);
+  const fetchOneHorse = async () => {
+    try {
+      const response = await fetch(`/horses/${id}`);
+      const data = await response.json();
+      setHorses(data);
 
-            if (data && data.owner) {
-                // Fetch owner details based on owner ID
-                const ownerResponse = await fetch(`/owners/${data.owner}`);
-                const ownerData = await ownerResponse.json();
-                setOwner(ownerData);
-              }
-        } catch (error) {
-            console.error('Error fetching horse:', error);
-        }
-    };
+      if (data && data.owner) {
+        // Fetch owner details based on owner ID
+        const ownerResponse = await fetch(`/owners/${data.owner}`);
+        const ownerData = await ownerResponse.json();
+        setOwner(ownerData);
+      }
 
+      if (data && data.location) {
+        // Fetch owner details based on location ID
+        const locationResponse = await fetch(`/locations/${data.location}`);
+        const locationData = await locationResponse.json();
+        console.log(locationData);
+      }
 
+    } catch (error) {
+      console.error('Error fetching horse:', error);
+    }
+  };
 
+  return (
 
-    return (
-
-            <div>
-              <h3>{horse.name}</h3>
-           <p>Owner: {owner.name}</p>
-              <p>Age: {horse.age}</p>
-              {horse.imageURL && (
-                <img src={horse.imageURL} alt={horse.name} />
-              )}
-            </div>
-          );
+    <div>
+      <h3>{horse.name}</h3>
+      <p>Owner: {owner.name}</p>
+      <p>Age: {horse.age}</p>
+      <p>Location: {location.locationName}</p>
+      <p>Breed: {horse.breed}</p>
+      <p>Height: {horse.height}</p>
+      <p>Born: {horse.year_of_birth}</p>
+      {horse.imageURL && (
+        <img src={horse.imageURL} alt={horse.name} />
+      )}
+    </div>
+  );
 
 }
 
